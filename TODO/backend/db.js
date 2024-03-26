c940e5f47cd23d3aed8e3b167c8b137e351fdaea
Maniteja0126 <maniteja2601@gmail.com>
2024-03-26 17:10:46 +0530

    Updated the mongoose schema

--- TODO/backend/db.js
+++ TODO/backend/db.js
@@ -6,7 +6,10 @@ mongoose.connect(process.env.DB_URI);
 const todoSchema = mongoose.Schema({
   title: String,
   description: String,
-  completed: Boolean,
+  completed: {
+    type: Boolean,
+    default : false
+  }
 });
 
 const todo = mongoose.model("todos", todoSchema);
